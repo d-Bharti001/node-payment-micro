c60b4774696dd3c44a18395432dc3d583201bf2a
Dharmveer Bharti <swansi.dharmveer@gmail.com>
2026-08-26 02:10:36 +0530

    Initial commit

---
+++ auth/proto/auth/User.ts
@@ -0,0 +1,12 @@
+// Original file: proto/auth.proto
+
+
+export interface User {
+  'seqId'?: (number);
+  'userId'?: (string);
+}
+
+export interface User__Output {
+  'seqId'?: (number);
+  'userId'?: (string);
+}
