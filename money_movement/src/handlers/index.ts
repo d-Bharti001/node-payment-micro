f5afeaa8593feb4b6940535e4df1116206298a70
Dharmveer Bharti <swansi.dharmveer@gmail.com>
2026-09-22 20:22:16 +0530

    Money movement: add getBalance service

--- money_movement/src/handlers/index.ts
+++ money_movement/src/handlers/index.ts
@@ -1,6 +1,8 @@
 import type { MoneyMovementServiceHandlers } from "proto/money_movement/MoneyMovementService";
+import { getBalance } from "src/handlers/balance/getBalance";
 import { transact } from "src/handlers/transaction/transact";
 
 export const MoneyMovementServiceHandler: MoneyMovementServiceHandlers = {
+    GetBalance: getBalance,
     Transact: transact,
 };
